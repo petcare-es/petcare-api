48d3e70c805247c25f2a5302e14d2974b0844e15
pab-h <dev.pab.2020@gmail.com>
2024-12-15 10:51:39 -0300

    feat: lógica de negócio de autenticação

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,5 +1,9 @@
 import UserRepository from "repository/interface/UserRepository";
 import UserType from "types/UserType";
+import ArgumentNotValidError from "./error/ArgumentNotValidError";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import env from "../env";
 
 type UserServiceRequest = {
     name: string;
@@ -9,6 +13,16 @@ type UserServiceRequest = {
 
 type UserServiceResponse = {
     user: UserType;
+}
+
+
+type UserAuthServiceRequest = {
+    email: string;
+    password: string;
+} 
+
+type UserAuthServiceResponse = {
+    token: string;
 }
 
 export default class UserService {
@@ -22,4 +36,38 @@ export default class UserService {
     public async create(req: UserServiceRequest): Promise<UserServiceResponse> {
         throw new Error("Not Implemented");
     }
+
+    public async auth(req: UserAuthServiceRequest): Promise<UserAuthServiceResponse> {
+        const { email, password } = req;
+
+        const userFound = await this.repository.findByEmail(email);
+
+        if (!userFound) {
+            throw new ArgumentNotValidError(
+                `O e-mail ${email} não está cadastrado`
+            );
+        }
+
+        const isSamePassword = await bcrypt.compare(
+            password, 
+            userFound.password
+        ); 
+
+        if (!isSamePassword) {
+            throw new ArgumentNotValidError(
+                "Senha inválida"
+            );
+        }
+
+        const token = jwt.sign(
+            { userId: userFound.id }, 
+            env.JWT_SECRET, 
+            { expiresIn: "1h" }
+        );
+
+        return { 
+            token 
+        };
+    }
+
 }
