84d0e66c539947869097bbb6e83c17d3acbd69b7
pab-h <dev.pab.2020@gmail.com>
2024-12-15 10:53:26 -0300

    test: Teste unitários de autenticação

---
+++ src/service/test/user/auth.spec.ts
@@ -0,0 +1,45 @@
+import UserInMemoryRepository from '../../../repository/inMemory/UserInMemoryRepository';
+import UserService from '../../../service/UserService';
+import ArgumentNotValidError from '../../../service/error/ArgumentNotValidError';
+
+import { describe, it, expect, beforeEach } from 'vitest';
+
+describe('Auth service', () => {
+    let userRepository: UserInMemoryRepository;
+    let userService: UserService;
+  
+    beforeEach(() => {
+      userRepository = new UserInMemoryRepository();
+      userService = new UserService(userRepository);
+    });
+  
+    it('it should be able to auth a user', async () => {
+        const { user } = await userService.create({
+            email: 'pedro@gmail.com',
+            name: 'Pedro',
+            password: '123456',
+        });
+
+      expect(async () => {
+        await userService.auth({
+            email: user.email, 
+            password: user.password 
+        });
+      }).not.toThrow();
+    });
+  
+    it('it should not be possible to authenticate with an invalid password', async () => {
+      const { user } = await userService.create({
+        name: 'Pedro',
+        email: 'pedro@gmail.com',
+        password: '123456',
+      });
+  
+      expect(async () => {
+        await userService.auth({
+          email: user.email,
+          password: "senha errada",
+        });
+      }).rejects.toBeInstanceOf(ArgumentNotValidError);
+    });
+  });
