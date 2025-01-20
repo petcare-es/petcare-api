f361e099ea28568418e9dca405836a3adf3103c1
pab-h <dev.pab.2020@gmail.com>
2025-01-19 22:06:43 -0300

    fix: ensureAuthentication

--- src/routes/pets.ts
+++ src/routes/pets.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import PetController from "controller/PetController";
+import ensureAuthentication from "middleware/ensureAuthentication";
 
 const petsRoutes = Router();
 const petController = new PetController();
+
+petsRoutes.use(ensureAuthentication);
 
 petsRoutes.post("/register/:id", petController.register);
 
