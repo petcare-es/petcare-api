833b5558a8504fac8fc8856200736eda5a9e3cf0
pab-h <dev.pab.2020@gmail.com>
2025-02-21 16:39:05 -0300

    feat: registros das rotas esquecidas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,16 @@ import { Router } from "express";
 
 import usersRoutes from "./users";
 import petsRoutes from "./pets";
+import vaccinesRoutes from "./vaccines";
+import appointmentsRoutes from "./appointments";
+import moodsRoutes from "./moods";
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
 routes.use("/pets", petsRoutes);
+routes.use("/vaccines", vaccinesRoutes);
+routes.use("/appointments", appointmentsRoutes);
+routes.use("/moods", moodsRoutes);
 
 export default routes;
