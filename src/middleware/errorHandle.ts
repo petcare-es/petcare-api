a813eb38714f73cb68f04337544ace2bfee88a26
pab-h <dev.pab.2020@gmail.com>
2025-02-26 22:54:53 -0300

    fix: lidando com o erro TokenAuthorizationMissingError

--- src/middleware/errorHandle.ts
+++ src/middleware/errorHandle.ts
@@ -1,55 +1,77 @@
 import { Prisma } from "@prisma/client";
 import type { NextFunction, Request, Response } from "express";
 import { TokenExpiredError } from "jsonwebtoken";
+import TokenAuthorizationMissingError from "service/error/TokenAuthorizationMissingError";
 import { ZodError } from "zod";
 
 export default function handleError(
   error: Error,
-  req: Request,
-  res: Response,
+  request: Request,
+  response: Response,
   next: NextFunction,
 ) {
   if (error instanceof ZodError) {
     console.error(error.errors);
 
-    return res.status(400).json({
+    return response.status(400).json({
       message: "Error de validação",
       issues: error.issues,
     });
   }
 
+  if (error instanceof TokenAuthorizationMissingError) {
+    console.error(error);
+    return response
+      .status(401)
+      .json({ message: "É necessário um token de autenticação" })
+  }
+
   if (error instanceof TokenExpiredError) {
     console.error(error);
-    return res.status(401).json({ message: "Token expirado" });
+    return response
+      .status(401)
+      .json({ message: "Token expirado" });
   }
 
   if (error instanceof Prisma.PrismaClientKnownRequestError) {
     console.error(error);
-    return res.status(400).json({ message: error.message });
+    return response
+      .status(400)
+      .json({ message: error.message });
   }
 
   if (error instanceof Prisma.PrismaClientUnknownRequestError) {
     console.error(error);
-    return res.status(500).json({ message: "Error interno do servidor" });
+    return response
+      .status(500)
+      .json({ message: "Error interno do servidor" });
   }
 
   if (error instanceof Prisma.PrismaClientInitializationError) {
     console.error(error);
-    return res.status(500).json({ message: error.message });
+    return response
+      .status(500)
+      .json({ message: error.message });
   }
 
   if (error instanceof Prisma.PrismaClientRustPanicError) {
     console.error(error);
-    return res.status(400).json({ message: error.message });
+    return response
+      .status(400)
+      .json({ message: error.message });
   }
 
   if (error instanceof Error) {
     console.error(error);
 
     if (error.message) {
-      return res.status(500).json({ message: error.message });
+      return response
+        .status(500)
+        .json({ message: error.message });
     }
 
-    return res.status(500).json({ message: "Error interno do servidor" });
+    return response
+      .status(500)
+      .json({ message: "Error interno do servidor" });
   }
 }
